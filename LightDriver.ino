b1354c294760e62b306ed4f06dd7d4b8c5e94068
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2017-04-15 15:45:39 -0400

    Move to FastLED

--- LightDriver.ino
+++ LightDriver.ino
@@ -1,54 +1,134 @@
+#include "FastLED.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <LPD8806.h>
 #include <SPI.h>
-#include <Thread.h>
-#include <ThreadController.h>
-#include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <SimpleTimer.h>
+
+//WATCHDOG SETUP
+SimpleTimer watchdog;
+#define MAX_ERRORS 3
+#define DELAY 3000
+
+void check_watchdog(uint8_t &errors) {
+  if (errors > MAX_ERRORS) {
+    Serial.println("!!");
+    Serial.println("Watchdog: Too many Errors, rebooting...");
+    ESP.restart();
+  }
+}
+
+void pet_watchdog() {
+  uint8_t error_count = 0;
+  while (!wifi_connect()) {
+    ++error_count;
+    check_watchdog(error_count);
+    Serial.println("WiFi failure");
+    delay(DELAY);
+  }
+  while (!mqtt_connect()){
+    ++error_count;
+    check_watchdog(error_count);
+    Serial.println("MQTT failure");
+    delay(DELAY);
+  }
+}
+
+//WIFI SETUP
+const String ssid = "";//SET THIS
+const String password = "";//SET THIS
+WiFiClient wifi;
+
+bool wifi_connect() {
+  if (wifi.connected()) {
+    return true;
+  }
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), password.c_str());
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    return true;
+  }
+  return false;
+}
+
+//LED STRIP SETUP
+#define NLEDS 480
+#define SCL 14
+#define MOSI 13
+
+CRGB leds[NLEDS];
+
+bool led_enable;
+
+void update_leds() {
+ FastLED.show();
+}
 
 //MQTT SETUP
 #define MQTT_SERVER      "192.168.2.1"
 #define MQTT_SERVERPORT  1883                   // use 8883 for SSL
+const char* MQTT_POWER = "lights/power";
+const char* MQTT_COLOR = "lights/color";
+const char* MQTT_EFFECT = "lights/effect";
+const char* MQTT_DATA = "lights/raw_leds";
 
-#define OTA_TIMEOUT 30000
-
-const String ssid = "";//SET THIS
-const String password = "";//SET THIS
-
-int nLEDs = 32*15;
-int sCL = 14;
-int mOSI = 13;
-LPD8806 strip = LPD8806(nLEDs, mOSI, sCL);
-
-void mqtt_callback(char *topic, uint8_t* payload, uint32_t len);
-
-WiFiClient wifi;
 PubSubClient client(MQTT_SERVER, MQTT_SERVERPORT, mqtt_callback, wifi);
 
-const char* MQTT_DATA = "lights/onoff/sub";
-const char* MQTT_POWER = "lights/data/pub";
-
-const float PHYSCIAL_MAX_POWER = 127.0f;
-float led_power;
-
-uint8_t colorMap(uint8_t color) {
-  return (uint8_t) led_power * (((float) color) * (PHYSCIAL_MAX_POWER / 255.0f));
+bool mqtt_connect() {
+  if (client.connected())
+  {
+    return true;
+  }
+  if (client.connect("BedroomLights")){
+    client.subscribe(MQTT_POWER);
+    client.subscribe(MQTT_COLOR);
+    client.subscribe(MQTT_EFFECT);
+    client.subscribe(MQTT_DATA);
+    return true;
+  }
+  return false;
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid.c_str(), password.c_str());
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
+bool is_topic(const char* t, const char *tt){
+  return strcmp(t, tt) == 0; 
+}
 
+void mqtt_callback(char *topic, uint8_t* payload, uint32_t len) {
+  Serial.print("MQTT: ");
+  Serial.println(topic);
+  uint8_t *safe_buffer = new uint8_t[len+1];
+  memcpy(safe_buffer, payload, len);
+  safe_buffer[len] = '\0';
+  if (is_topic(topic, MQTT_POWER)) {
+    LEDS.setBrightness(atoi((char*)safe_buffer));
+    Serial.println(atoi((char*)safe_buffer));
+  } else if (is_topic(topic, MQTT_COLOR)) {
+    Serial.println((char*)safe_buffer);
+    fill_solid(leds, NLEDS, CRGB(strtol((char*)&safe_buffer[1], NULL, 16)));
+  } else if (is_topic(topic, MQTT_EFFECT)) {
+    Serial.println((char*)payload);
+    Serial.println("Effects not implemented");
+  } else if (is_topic(topic, MQTT_DATA)) {
+    if (((len - 4) % 3) != 0) {
+      Serial.println("Data stream not aligned!");
+      return;
+    }
+    //Parse Color instruction struct
+    uint16_t start_p = payload[0] + (payload[1] << 8);
+    uint16_t end_p = payload[2] + (payload[3] << 8);
+    if (len < end_p + 4) {
+      Serial.println("Data stream too short!");
+    }
+    for(uint16_t i = start_p; i < end_p; ++i) {
+      leds[i] = CRGB(payload[4+i], payload[5+i], payload[6+i]);
+    }
+  }
+}
+
+//OTA setup
+void setup_ota() {
   // Port defaults to 8266
   ArduinoOTA.setPort(8266);
 
@@ -76,64 +156,46 @@ void setup() {
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
-  Serial.println("Ready");
+}
+  
+//==ENTRY==
+void connect_all() {
+  uint8_t error_count = 0;
+  Serial.print("Connecting Wifi...");
+  while (!wifi_connect()) {
+    ++error_count;
+    check_watchdog(error_count);
+    Serial.print("X");
+    delay(DELAY);
+  }
+  Serial.println("Ok");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  strip.begin();
-  strip.show();
-  if (client.connect("BedroomLights")){
-    Serial.print("MQTT Connected!");
-    client.subscribe("lights/onoff");
-    client.subscribe("lights/data");
-  } else {
-    Serial.println("MQTT Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+  Serial.print("Connecting MQTT...");
+  while (!mqtt_connect()){
+    ++error_count;
+    check_watchdog(error_count);
+    Serial.print("X");
+    delay(DELAY);
   }
+  Serial.println("Ok");
 }
 
-void mqtt_callback(char *topic, uint8_t* payload, uint32_t len) {
-  if (strcmp(topic, "lights/onoff") == 0) {
-    if (len > 0) {
-      uint8_t p = atoi((char*)payload);
-      if (p > 0) {
-        led_power = ((float)p) / 255.0f;
-      } else {
-        led_power = 0.0f;
-        for(uint16_t i = 0; i < nLEDs; ++i) {
-          strip.setPixelColor(i, 0, 0, 0);
-        }
-      }
-      strip.show();
-    }
-  } else if (strcmp(topic, "lights/data") == 0) {
-    if (len <= 9) {
-      int v = (int) strtol(&(((char*)payload)[1]), NULL, 16);
-      Serial.println(v);
-      uint8_t r = colorMap((v >> 16) & 0xFF);
-      uint8_t g = colorMap((v >> 8) & 0xFF);
-      uint8_t b = colorMap(v & 0xFF);
-      Serial.printf("%d %d %d\n", r,g,b);
-      for(uint16_t i = 0; i < nLEDs; ++i) {
-        strip.setPixelColor(i, r, g, b);
-      }
-    } else {
-      //Parse Color instruction struct
-      uint8_t start_p = payload[0];
-      uint8_t len = payload[1];
-      for(uint8_t i = start_p; i < start_p + len; ++i) {
-        strip.setPixelColor(i, colorMap(payload[2+i]), colorMap(payload[3+i]), colorMap(payload[4+i]));
-      }
-    }
-    strip.show();
-  }
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Boot");
+  
+  LEDS.addLeds<LPD8806, MOSI, SCL, RGB>(leds, NLEDS);
+  connect_all();
+  
+  setup_ota();
+  Serial.println("System Up");
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  pet_watchdog();
   client.loop();
-  if ((OTA_TIMEOUT > 0) && (millis() < OTA_TIMEOUT)) {
-    ArduinoOTA.handle();
-  }
+  update_leds();
 }
-
 
